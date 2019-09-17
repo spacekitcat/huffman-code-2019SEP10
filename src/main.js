a3766ff2e4df85455bb8bc413d60b96504c899e3
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-09-17 19:40:35 +0100

    Improved Jest setup, improved eslint setup, fixing some less than optimal setup in the package.json, adding some test resources, updating the .gitignore, adding a module for counting char occurences via the nodejs stream interface and tests.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import fs from 'fs';
+import process from 'process';
+
 const SayHello = () => {
+  const fileName = process.argv[2];
+  fs.createReadStream(fileName);
   console.log("Hello World!");
 }
 
