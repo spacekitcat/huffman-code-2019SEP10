c52e47a9dd8862162b935e39c07638be47627972
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-09-18 20:24:58 +0100

    feat(append): Adding functionality to add lower and higher priority nodes to the heap with a depth of 1 below root.

--- __tests__/appendNodeToHeap.test.js
+++ __tests__/appendNodeToHeap.test.js
@@ -1,7 +1,36 @@
 import appendNodeToHeap from '../src/appendNodeToHeap';
+import Node from '../src/Node';
 
 describe('HeapAppend', () => {
-  it('works', () => {
-    appendNodeToHeap();
+  describe('Adding a single child to a single root', () => {
+    describe('Adding a higher priority child', () => {
+      let rootNode;
+      let childNode;
+      beforeAll(() => {
+        rootNode = new Node(2, Buffer.from('y'));
+        childNode = new Node(8, Buffer.from('a'));
+
+        appendNodeToHeap(rootNode, childNode);
+      });
+
+      it('has the expected right child', () => {
+        expect(rootNode.getRightChild()).toMatchObject(childNode);
+      });
+    });
+
+    describe('Adding a lower priority child', () => {
+      let rootNode;
+      let childNode;
+      beforeAll(() => {
+        rootNode = new Node(5, Buffer.from('y'));
+        childNode = new Node(4, Buffer.from('a'));
+
+        appendNodeToHeap(rootNode, childNode);
+      });
+
+      it('has the expected left child', () => {
+        expect(rootNode.getLeftChild()).toMatchObject(childNode);
+      });
+    });
   });
 });
--- src/appendNodeToHeap.js
+++ src/appendNodeToHeap.js
@@ -1,3 +1,7 @@
-export default () => {
-  
-}
+export default (rootNode, childNode) => {
+  if (childNode.getValue() > rootNode.getValue()) {
+    rootNode.setRightChild(childNode);
+  } else {
+    rootNode.setLeftChild(childNode);
+  }
+};
