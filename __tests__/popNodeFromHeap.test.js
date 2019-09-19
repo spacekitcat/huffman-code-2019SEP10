b093bba5cb1cfec6f911eb543a4ea60d80d8a17a
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-09-19 21:24:17 +0100

    feat(pop): Added basic pop function, working to a depth of 1.

--- __tests__/popNodeFromHeap.test.js
+++ __tests__/popNodeFromHeap.test.js
@@ -1,17 +1,87 @@
 import popNodeFromHeap from '../src/popNodeFromHeap';
 import Node from '../src/Node';
+import appendNodeToHeap from '../src/appendNodeToHeap';
 
 describe('HeapAppend', () => {
   describe('Pop from null', () => {
     it('returns null', () => {
-      expect(popNodeFromHeap(null)).toBe(null);
+      expect(popNodeFromHeap(null)).toMatchObject({
+        returned: null,
+        newRoot: null
+      });
     });
   });
 
-  describe('Pop from single lead tree', () => {
-    it('returns null', () => {
-      const rootNode = new Node(2, 'h');
-      expect(popNodeFromHeap(rootNode)).toMatchObject(rootNode);
+  describe('Pop from single leaf tree', () => {
+    let rootNode;
+    let popValue;
+    beforeAll(() => {
+      rootNode = new Node(2, 'h');
+      popValue = popNodeFromHeap(rootNode);
+    });
+
+    it('returns the pop values', () => {
+      expect(popValue).toMatchObject({ returned: rootNode, newRoot: null });
+    });
+  });
+
+  describe('Pop from a tree with a higher priority child', () => {
+    let rootNode;
+    let childNode;
+    let popValue;
+    beforeAll(() => {
+      rootNode = new Node(2, 'h');
+      childNode = new Node(3, 'g');
+      appendNodeToHeap(rootNode, childNode);
+      popValue = popNodeFromHeap(rootNode);
+    });
+
+    it('returns the pop values', () => {
+      expect(popValue).toMatchObject({
+        returned: childNode,
+        newRoot: rootNode
+      });
+    });
+  });
+
+  describe('Pop from a tree with a lower priority child', () => {
+    let rootNode;
+    let childNode;
+    let popValue;
+    beforeAll(() => {
+      rootNode = new Node(5, 'x');
+      childNode = new Node(1, 'x');
+      appendNodeToHeap(rootNode, childNode);
+      popValue = popNodeFromHeap(rootNode);
+    });
+
+    it('returns the pop values', () => {
+      expect(popValue).toMatchObject({
+        returned: rootNode,
+        newRoot: childNode
+      });
+    });
+  });
+
+  describe('Pop from a tree with a higher priority child and a lower priority child', () => {
+    let rootNode;
+    let childNodeHigher;
+    let childNodeLower;
+    let popValue;
+    beforeAll(() => {
+      rootNode = new Node(2, 'a');
+      childNodeHigher = new Node(3, 'c');
+      appendNodeToHeap(rootNode, childNodeHigher);
+      childNodeLower = new Node(1, 'q');
+      appendNodeToHeap(rootNode, childNodeLower);
+      popValue = popNodeFromHeap(rootNode);
+    });
+
+    it('returns the pop values', () => {
+      expect(popValue).toMatchObject({
+        returned: childNodeHigher,
+        newRoot: rootNode
+      });
     });
   });
 });
--- src/popNodeFromHeap.js
+++ src/popNodeFromHeap.js
@@ -1,3 +1,19 @@
-export default rootNode => {
-  return rootNode;
-};
+export default function popNodeFromHeap(rootNode) {
+  if (!rootNode) {
+    return { returned: null, newRoot: null };
+  }
+
+  if (rootNode.getRightChild()) {
+    if (!rootNode.getRightChild().getRightChild()) {
+      const returned = rootNode.getRightChild();
+      rootNode.setRightChild(null);
+      return { returned, newRoot: rootNode };
+    }
+  }
+
+  if (rootNode.getLeftChild()) {
+    return { returned: rootNode, newRoot: rootNode.getLeftChild() };
+  }
+
+  return { returned: rootNode, newRoot: null };
+}
