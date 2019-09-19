8284e199b9967fada62510db8df82816454c2637
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-09-19 20:41:14 +0100

    feat(pop): Added half the functionality for doing pop at a depth of 0.

--- __tests__/popNodeFromHeap.test.js
+++ __tests__/popNodeFromHeap.test.js
@@ -1,7 +1,17 @@
 import popNodeFromHeap from '../src/popNodeFromHeap';
+import Node from '../src/Node';
 
 describe('HeapAppend', () => {
-  it('works', () => {
-    popNodeFromHeap();
+  describe('Pop from null', () => {
+    it('returns null', () => {
+      expect(popNodeFromHeap(null)).toBe(null);
+    });
+  });
+
+  describe('Pop from single lead tree', () => {
+    it('returns null', () => {
+      const rootNode = new Node(2, 'h');
+      expect(popNodeFromHeap(rootNode)).toMatchObject(rootNode);
+    });
   });
 });
